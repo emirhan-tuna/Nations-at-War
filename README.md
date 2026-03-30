adfcaa30c7a56fbb0da31dd755296e72600fe639
Javidan <javidangaragezov@gmail.com>
2026-03-30 21:52:32 +0300

    Early implementation of menu items
    Added seperate folder called "menu_items" inside the assets folder to keep it organized.
    
    Did an early implementation of menu items to see what it would look like.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Nations-at-War
+An online 1v1 strategy game.
--- core/src/main/java/tr/edu/bilkent/cs102/game/Main.java
+++ core/src/main/java/tr/edu/bilkent/cs102/game/Main.java
@@ -1,32 +1,72 @@
 package tr.edu.bilkent.cs102.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    Stage stage;
+    Texture bgTexture;
+    Table table;
+
+    ImageButton playButton;
+    ImageButton settingsButton;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        bgTexture = new Texture(Gdx.files.internal("menu_items/background.jpg"));
+        Texture playTex = new Texture(Gdx.files.internal("menu_items/play_button.png"));
+        Texture settingsTex = new Texture(Gdx.files.internal("menu_items/settings_button.png"));
+
+        TextureRegionDrawable playDrawable = new TextureRegionDrawable(new TextureRegion(playTex));
+        TextureRegionDrawable settingsDrawable = new TextureRegionDrawable(new TextureRegion(settingsTex));
+
+        playButton = new ImageButton(playDrawable);
+        settingsButton = new ImageButton(settingsDrawable);
+
+        table = new Table();
+        table.setFillParent(true);
+        table.center();
+
+        table.add(playButton).size(300, 100).padBottom(20).row();
+        table.add(settingsButton).size(300, 100);
+
+        stage.addActor(table);
+
+        playButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("LOG: Play Button Clicked!");
+            }
+        });
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        ScreenUtils.clear(0, 0, 0, 1);
+
+        stage.getBatch().begin();
+        stage.getBatch().draw(bgTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        stage.getBatch().end();
+
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        stage.dispose();
+        bgTexture.dispose();
     }
 }
