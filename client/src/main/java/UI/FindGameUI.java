89ceca1e15b965a3634018143cb1e21d2d3718a3
emirhan-tuna <ekimtuna1307@gmail.com>
2026-04-12 17:57:43 +0300

    Server fix again

--- client/src/main/java/UI/FindGameUI.java
+++ client/src/main/java/UI/FindGameUI.java
@@ -97,34 +97,36 @@ public class FindGameUI implements Screen{
                 System.out.println("Server status");
 
                 if (serverStatus == 0) {
-                    if (gameFound) {
-                        task.cancel();
+                    if (gameFound) return;
+                    gameFound = true;
 
-                        Gdx.app.postRunnable(new Runnable() {
-                            @Override
-                            public void run() {
-                                JsonValue serverInfo = file.get("server");
-                                int gameId = serverInfo.getInt("gameId");
-                                String host = serverInfo.getString("host");
-                                int port = serverInfo.getInt("port");
+                    task.cancel();
 
-                                GameScreenUI newUI = new GameScreenUI(game, networkManage);
-                                ClientGameManager manager = newUI.getClientManager();
+                    Gdx.app.postRunnable(new Runnable() {
+                        @Override
+                        public void run() {
+                            JsonValue serverInfo = file.get("server");
+                            int gameId = serverInfo.getInt("gameId");
+                            String host = serverInfo.getString("host");
+                            int port = serverInfo.getInt("port");
 
-                                networkManage.connect(host, port, gameId, game.userToken, manager);
-                                game.setScreen(newUI);
+                            GameScreenUI newUI = new GameScreenUI(game, networkManage);
+                            ClientGameManager manager = newUI.getClientManager();
 
-                            }
-                        });
-                    } else if (serverStatus == 2) {
-                        Gdx.app.postRunnable(new Runnable() {
-                            @Override
-                            public void run() {
-                                game.setScreen(new MainMenuUi(game, stage, game.skin));
-                            }
-                        });
-                    }  
-                }  
+                            networkManage.connect(host, port, gameId, game.userToken, manager);
+                            game.setScreen(newUI);
+
+                        }
+                    });
+                } else if (serverStatus == 2) {
+                    Gdx.app.postRunnable(new Runnable() {
+                        @Override
+                        public void run() {
+                            game.setScreen(new MainMenuUi(game, stage, game.skin));
+                        }
+                    });
+                }
+                
             }
             @Override
             public void failed(Throwable t) {}
